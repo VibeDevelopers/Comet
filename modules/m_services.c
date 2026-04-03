8ebb38364d91352ef92c04b05073f1ab4e481584
TwinUsers <etdsaunders@gmail.com>
2026-04-03 12:48:30 +0100

    Tidy up module loading code The existing code had several defects:
    - Core module file names were hardcoded, while autoloaded non-
      core module filenames were not.  Code that loaded core modules
      had a different but in some places duplicated structure to the
      code that loaded autoloaded modules.
    
    - Module loading for autoloaded modules happened in dentry
      order, making it unpredictable from one server to the next, or
      even across instances of `make install` on the same server.
    
    - Module loading for autoloaded modules did not verify that the
      dentry was a file before trying to load it.
    
    - An IRCd start (or a conftest) only printed lines about loading
      modules mentioned in the configuration file.
    
    - The configuration file was parsed before loading core and
      autoloaded modules, meaning any modules specified in the
      configuration file would be loaded first.
    
    - Once a module was loaded, it was added to the head of the
      module list, making MODLIST show them in reverse loaded order
      (newest (re)loaded modules at the top of the list).
    
    These are addressed as follows:
    
    - We now scan the core and autoload module directories and use
      this to determine which modules to load.
    
    - We now sort the dentries according to their filename before
      iterating them to load them, making module load order finally
      deterministic.
    
    - We now test if a dentry is actually a file before trying to
      load it.
    
    - We now print a message to the console (if running in
      foreground or conftest mode) when loading any module,
      including core and autoloaded modules.
    
    - The message printed to the console now has the IRCd module
      directory prefix stripped from it if it matches.
    
    - The configuration file is now parsed after loading core and
      autoload modules.
    
    - The modules are added to the module list in the order that
      they are loaded. This now makes MODLIST show the order that
      modules were (re)loaded in.
    
    Modifications to two modules are part of this effort:
    
    - The autoload module `m_alias` relied upon the alias dict not
      being NULL when loaded, but this is only the case after the
      configuration file has been parsed.
    
      The module already had a "rehash" hook to destroy all existing
      aliases and create new ones, so we can just make the module do
      nothing on load (remove its modinit function) and change it to
      use the "conf_read_end" hook instead of the "rehash" hook, and
      it will still create aliases after the configuration file is
      subsequently parsed.
    
      With this modification, there are now no in-tree consumers of
      the "rehash" hook.
    
    - The autoload module `m_services` iterated `service_list` upon
      loading it, which will now be empty (a no-op) since the module
      is loaded before configuration file is parsed.
    
      This module too handles the configuration file being parsed
      with a "conf_read_end" hook and takes the appropriate action,
      so we can just remove this code from its modinit function.
    
    All other modules' modinit functions have been audited for
    behaviour like this; there was nothing of note.

--- modules/m_services.c
+++ modules/m_services.c
@@ -107,7 +107,6 @@ DECLARE_MODULE_AV2(services, _modinit, _moddeinit, services_clist, services_hlis
 static int
 _modinit(void)
 {
-	mark_services();
 	add_isupport("FNC", isupport_string, "");
 	return 0;
 }
